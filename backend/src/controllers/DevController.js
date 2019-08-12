28510fcb76c8b1cd29f7e82a14a66c1de184bf6a
Luiz Henrique da silva <masteraioria@gmail.com>
2019-08-11 23:50:30 -0300

    initial commit

---
+++ backend/src/controllers/DevController.js
@@ -0,0 +1,43 @@
+const axios = require('axios');
+const Dev = require('../models/Dev');
+
+module.exports = {
+    async index(req, res){
+        const {user} = req.headers;
+        
+        const loggedDev = await Dev.findById(user);
+
+        const users = await Dev.find({
+           $and: [
+               { _id: { $ne: user } }, // ne = not equal
+               { _id: { $nin: loggedDev.likes } }, // nin = not in, para que não tragam os usuarios que o usuario logado já deu like
+               { _id: { $nin: loggedDev.dislikes } }, // nin = not in, para que não tragam os usuarios que o usuario logado já deu dislike
+           ],
+        })
+
+        return res.json(users);
+    },
+
+    async store(req, res){
+        const {username} = req.body;
+
+        const userExists = await Dev.findOne({user: username});
+
+        if(userExists){
+            return res.json(userExists);
+        }
+
+        const response = await axios.get(`https://api.github.com/users/${username}`)
+
+        const { name, bio, avatar_url: avatar } = response.data;
+
+        const dev = await Dev.create({
+            name, 
+            user: username, 
+            bio, 
+            avatar
+        })
+
+        return res.json(dev);
+    }
+};
